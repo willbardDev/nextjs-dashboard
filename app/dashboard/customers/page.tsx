94f7010864fe3e6c471bdd9e36c6a27bd975074b
Willbard Beatus Mloka <130184031+willbardDev@users.noreply.github.com>
2025-01-29 17:02:24 +0300

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <p>Customers page</p>
+  )
+}
+
+export default page
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <p>Invoices page</p>
+  )
+}
+
+export default page

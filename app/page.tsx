bf0ad6fc10df258fbc05f7bf8c621feae8f8a7e9
Willbard Beatus Mloka <130184031+willbardDev@users.noreply.github.com>
2025-01-29 18:03:55 +0300

    add database

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Page() {
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
-            , brought to you by Vercel.
+            , brought to you by Vercel
           </p>
           <Link
             href="/login"

b6f6005f2bf2b6bae23a0c81f2c723668ce3e67d
Willbard Beatus Mloka <130184031+willbardDev@users.noreply.github.com>
2025-02-03 08:57:12 +0300

    using ZOD for validation

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -24,7 +24,6 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue=""
-              // onChange={}
             >
               <option value="" disabled>
                 Select a customer
